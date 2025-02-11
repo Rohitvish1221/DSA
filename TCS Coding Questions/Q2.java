d2532f6283b6435445c1aaac2d18da0233e74ef2
Your Name <Rohit@vishro1221@gmail.com>
2025-02-11 18:36:32 +0530

    Edit Done

--- TCS Coding Questions/Q2.java
+++ TCS Coding Questions/Q2.java
@@ -1,12 +1,14 @@
+
 /* Q1: Find the Largest element in an array. */
 /* Time Complexity --> O(n*logn) */
 import java.util.Arrays;
 
 public class Q2 {
-    static int largestNumber(int[] arr){
+    static int largestNumber(int[] arr) {
         Arrays.sort(arr);
-        return arr[arr.length-1];
+        return arr[arr.length - 1];
     }
+
     public static void main(String[] args) {
         int arr[] = { 22, 4, 6, 11, 15 };
         System.out.println("Smallest Number : " + largestNumber(arr));
