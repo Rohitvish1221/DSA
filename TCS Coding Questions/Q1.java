e64fdd64316c3693feabd2a3448e030702b7378d
Your Name <Rohit@vishro1221@gmail.com>
2025-02-11 12:56:52 +0530

    Completed Q1

---
+++ TCS Coding Questions/Q1.java
@@ -0,0 +1,18 @@
+/* Q1: Find the smallest element in an array. */
+
+public class Q1 {
+
+    static int smallestNumber(int[] arr) {
+        int ans = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1])
+                ans = arr[i+1];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 22, 4, 6, 11, 15 };
+        System.out.println("Smallest Number : " + smallestNumber(arr));
+    }
+}
