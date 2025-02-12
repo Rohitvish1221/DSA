da92cec1d951a05fa257928c33b7cd7cafd5c7ad
Your Name <Rohit@vishro1221@gmail.com>
2025-02-12 18:44:05 +0530

    Completed Q4

---
+++ TCS Coding Questions/Q4.java
@@ -0,0 +1,20 @@
+/* Q4: Reverse a given array */
+/* Time Complexity --> O(n) */
+
+public class Q4 {
+
+    static void reverseArray(int[] arr, int n) {
+        int i = 0;
+        while (n > 0) {
+            System.out.println("Index[" + i + "] :" + arr[--n]);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 22, 4, 6, 11, 15 };
+        int len = arr.length;
+        reverseArray(arr, len);
+    }
+}
+/* Another Method --> Collections.reverse(Arrays.asList(arr)) */
