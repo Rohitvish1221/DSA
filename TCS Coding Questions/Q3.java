e4f1c7ad79fa92ace6495541b9c8ecd45cd08607
Your Name <Rohit@vishro1221@gmail.com>
2025-02-12 09:04:38 +0530

    Q3 done

--- TCS Coding Questions/Q3.java
+++ TCS Coding Questions/Q3.java
@@ -5,34 +5,34 @@ public class Q3 {
 
     static int[] secondLargestAndSmallest(int arr[], int n) {
         if (n <= 1) {
-            return new int[]{-1,-1};
+            return new int[] { -1, -1 };
         }
-        
+
         int large = Integer.MIN_VALUE;
         int scndLarge = Integer.MIN_VALUE;
         int small = Integer.MAX_VALUE;
         int scndSmall = Integer.MAX_VALUE;
 
-        for(int i=0;i<n;i++){
-            if( arr[i] > large){
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > large) {
                 scndLarge = large;
                 large = arr[i];
             }
-            
-            if(arr[i] > scndLarge && arr[i] != large){
+
+            if (arr[i] > scndLarge && arr[i] != large) {
                 scndLarge = arr[i];
             }
 
-            if( arr[i] < small){
+            if (arr[i] < small) {
                 scndSmall = small;
                 small = arr[i];
             }
-            
-            if(arr[i] < scndSmall && arr[i] != small){
+
+            if (arr[i] < scndSmall && arr[i] != small) {
                 scndSmall = arr[i];
             }
         }
-        return new int[]{scndSmall , scndLarge};
+        return new int[] { scndSmall, scndLarge };
     }
 
     public static void main(String[] args) {
