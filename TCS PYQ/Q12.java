e6023a4c36f1c4b91506aadd7f8a94a593bc45ee
Your Name <Rohit@vishro1221@gmail.com>
2025-05-14 14:22:23 +0530

    not done yet

---
+++ TCS PYQ/Q12.java
@@ -0,0 +1,27 @@
+/* TCS NQT Coding Question – 12
+Problem Statement – Given an array of integers and a sum, the task is to count all subsets of given array with sum equal to given sum.
+Input: The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case contains an integer n denoting the size of the array. The next line contains space separated integers forming the array. The last line contains the sum.
+Output: Count all the subsets of given array with sum equal to given sum.
+NOTE: Since the result can be very large, print the value modulo 109+7.
+Constraints: 1<=T<=100 1<=n<=103
+1<=a[i]<=103
+1<=sum<=103
+Example : Input : 2
+6
+2 3 5 6 8 10
+10
+5
+1 2 3 4 5
+10
+Output :
+3
+3
+Explanation :
+Testcase 1: possible subsets : (2,3,5) , (2,8) and (10)
+Testcase 2: possible subsets : (1,2,3,4) , (2,3,5) and (1,4,5) */
+
+public class Q12 {
+    public static void main(String[] args) {
+        
+    }
+}
